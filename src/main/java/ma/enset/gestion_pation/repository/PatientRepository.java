87023af9af2898d70ceb134d16a41d0d71deeb31
AzmiZakaria <zakariaazmi45@gmail.com>
2025-05-13 23:05:03 +0100

    test operation sur entity Patient

--- src/main/java/ma/enset/gestion_pation/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.enset.gestion_pation.repository;
-
-import ma.enset.gestion_pation.entities.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
